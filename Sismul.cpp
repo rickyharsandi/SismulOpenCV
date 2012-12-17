93762ccb33956f99d74cf105dd9a4ba85bf1f0e5
Boy Sandy Gladies Arriezona <reno.esper@gmail.com>
2012-12-17 18:40:38 +0700

    Menambah fungsi baru

--- Sismul.cpp
+++ Sismul.cpp
@@ -18,10 +18,29 @@
 
 using namespace cv;
 
+bool BacaFileVideo(char* namafile);
+int BacaFrameRate();
+int BacaJumlahFrame();
+int BacaLebar();
+int BacaTinggi();
+int BacaIndexFrame();
+void PergiKeFrame(int);
+void StartVideo();
+void StartVideo(int awal, int akhir);
+void StopVideo();
+void UbahVideo(int awal, int akhir, char* lokasi);
+void SelisihkanFrames(int awal,int akhir);
+
+int totalframe;
+CvCapture* pointerscreenshot;
+
 int main(int argc, char** argv) {
-	cvNamedWindow("OpenCV Video", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture("cursor.avi");
+	char* namafile;
 	IplImage* frame;
+
+	cvNamedWindow("OpenCV Video", CV_WINDOW_AUTOSIZE);
+/*	CvCapture* capture = cvCreateFileCapture("cursor.avi");
+
 	while (1) {
 
 		frame = cvQueryFrame(capture);
@@ -33,6 +52,29 @@ int main(int argc, char** argv) {
 		if (c == 27)
 			break;
 	}
+*/
 	cvReleaseCapture(&capture);
 	cvDestroyWindow("OpenCV Video");
+}
+
+bool BacaFileVideo(char* namafile)
+{
+	pointerscreenshot = cvCaptureFromFile(namafile);
+	if(!namafile)
+		return false;
+	if(!pointerscreenshot)
+		return false;
+
+	totalframe = (int)cvGetCaptureProperty(pointerscreenshot,CV_CAP_PROP_FRAME_COUNT);
+	return true;
+}
+
+int BacaFrameRate()
+{
+	return (int)cvGetCaptureProperty(pointerscreenshot,CV_CAP_PROP_FPS);
+}
+
+int BacaJumlahFrame()
+{
+	return totalframe;
 }
