7267073d7330f0e6d25237fc8b0195320c8b79eb
Boy Sandy Gladies Arriezona <reno.esper@gmail.com>
2013-01-04 08:09:43 +0700

    Sintak tema yang baru

--- Sismul.cpp
+++ Sismul.cpp
@@ -13,30 +13,79 @@
 #include <cxmisc.h>
 #include <ml.h>
 
- int main() {
-   CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
-   if ( !capture ) {
-     fprintf( stderr, "ERROR: capture is NULL \n" );
-     getchar();
-     return -1;
-   }
+int  main(){
+	CvCapture* capture;
+	capture = cvCaptureFromCAM(CV_CAP_ANY);
 
-   cvNamedWindow( "OpenCV - Sistem Multimedia", CV_WINDOW_AUTOSIZE );
+	//Mengecek ketersediaan kamera
+	if (!capture){
+		fprintf(stderr, "ERROR: Kamera tidak terdeteksi\n");
+	}
 
-   while ( 1 ) {
+	IplImage* frame0;
+	IplImage* frame1;
+	IplImage* movingframe;
+	IplImage* grayframe0;
+	IplImage* grayframe1;
+	IplImage* graymovingframe;
 
-     IplImage* frame = cvQueryFrame( capture );
-     if ( !frame ) {
-       fprintf( stderr, "ERROR: frame is null...\n" );
-       getchar();
-       break;
-     }
-     cvShowImage( "OpenCV - Sistem Multimedia", frame );
+	//Mengekstrak frame dari video
+	frame1 = cvQueryFrame(capture);
 
-     if ( (cvWaitKey(10) & 255) == 27 ) break;
-   }
+	//Mengecek ketersediaan frame
+	if(!frame1)
+		return -1;
 
-   cvReleaseCapture( &capture );
-   cvDestroyWindow( "OpenCV - Sistem Multimedia" );
-   return 0;
- }
+	//Meng-clone frame dari frame1 ke frame0
+	frame0= cvCloneImage(frame1);
+
+	//Mengambil properti-properti frame
+	int column = frame1->height;
+	int row = frame1->width;
+	int depth = frame1->depth;
+
+	//Mengalokasikan memori untuk gambar
+	movingframe = cvCreateImage(cvSize(row,column),depth,3);
+	grayframe0 = cvCreateImage(cvSize(row,column),depth,1);
+	grayframe1 = cvCreateImage(cvSize(row,column),depth,1);
+	graymovingframe = cvCreateImage(cvSize(row,column),depth,1);
+
+	//Menampilkan gambar yang ditangkap dari kamera
+	while(1){
+		//Mengekstrak frame 2 ke frame1
+		frame1 = cvQueryFrame(capture);
+
+		//Konversi dari RGB ke Gray
+		cvCvtColor(frame0,grayframe0,CV_RGB2GRAY);
+		cvCvtColor(frame1,grayframe1,CV_RGB2GRAY);
+
+		//Menghitung perbedaan antar-frame
+		cvAbsDiff(grayframe0,grayframe1,graymovingframe);
+		cvAbsDiff(frame0,frame1,movingframe);
+
+		//Menampilkan gambar asli
+		cvShowImage("Original Image",frame1);
+
+		//Menampilkan perbedaan antar-frame dari gambar-gambar Gray
+		cvShowImage("Gray Image Difference ",graymovingframe);
+
+		//Menampilkan perbedaan antar-frame dari gambar-gambar RGB
+		cvShowImage("RGB Image Difference ",movingframe);
+
+		//Keluar dari looping program menggunakan tombol escape
+		if ((cvWaitKey(10) & 255) == 27)
+			break;
+
+		//Mengkopi frame1 ke frame0
+		cvCopy(frame1, frame0);
+	}
+
+	//Membersihkan memori yang telah digunakan
+	cvReleaseCapture(&capture);
+	cvReleaseImage(&grayframe0);
+	cvReleaseImage(&grayframe1);
+	cvReleaseImage(&graymovingframe);
+	cvReleaseImage(&frame1);
+
+	return 0;
+}
